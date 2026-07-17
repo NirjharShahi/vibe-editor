31b84ef8e697e7a456ba2ae4b6aba0ca8f2d9b54
Nirjhar Shahi <shahinirjhar2005@gmail.com>
2026-07-17 20:38:38 +0530

    02 Database

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const user = db.user;
   return (
     <div>
       <Button>Get Started.</Button>
